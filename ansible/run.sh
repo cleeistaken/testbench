a8fdf026e442eeab8470b3404e2c72f63157fa24
root <root@testbench.localdomain>
2022-01-09 15:01:47 -0500

    found the issue with centos 8.x and python > 3.6 with firewalld

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash 
 
-# Activate the virtual environment
-echo "Activating the environment"
-source $HOME/.python3-venv/bin/activate
-
 # Configure the systems
 ANSIBLE_LOCALHOST_WARNING=false
 export ANSIBLE_LOCALHOST_WARNING
-~/.python3-venv/bin/ansible-playbook main.yml
+ansible-playbook main.yml
