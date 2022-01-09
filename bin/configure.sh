c227d05d9650ad726ec45c02592ada0a933a83a3
root <root@testbench.localdomain>
2022-01-09 15:27:45 -0500

    fixed the problem with centos 8.x and firewalld pythong3 bindings

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,37 +1,23 @@
 #! /bin/bash
 
-#
-# Install system packages
-echo "Installing system packages"
-sudo yum -y install git
-
-#
 # Install Terraform
 echo "Installing Terraform"
-#./install_terraform.sh
+./install_terraform.sh
 
 #
 # Install Python
-echo "Installing Python"
-#./install_python.sh
+echo "Installing Python3"
+yum -y install python3
 
-#
-# Configure Python
-echo "Creating Python virtual environment"
-python3 -m venv $HOME/.python3-venv
-
-# Activate the virtual environment
-echo "Activating the virtual environment"
-source $HOME/.python3-venv/bin/activate
 
 # Upgrade python packages
 echo "Upgrading pip and setuptools"
-pip install --upgrade pip
-pip install --upgrade setuptools
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
 
 # Install required packages"
 echo "Installing required packages"
-pip install wheel ansible jmespath netaddr firewalld
+pip install wheel ansible jmespath netaddr
 
 # Configure the systems
 pushd ../ansible > /dev/null
