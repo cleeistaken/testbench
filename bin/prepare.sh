8fca29f3f5839b421a0dac10167f6829b735c385
cleeistaken <cleeistaken@gmail.com>
2022-01-10 15:48:42 -0500

    add fstrim to system preparation script

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -13,9 +13,26 @@ echo -n > /etc/machine-id
 rm /var/lib/dbus/machine-id
 ln -s /etc/machine-id /var/lib/dbus/machine-id
 
+file="~/.bash_history"
+if test -f "$file"; then
+  echo "Deleting bash history"
+  rm -f "$file"
+fi
+
+file="~/.ssh/known_hosts"
+if test -f "$file"; then
+  echo "Deleting known hosts"
+  rm -f "$file"
+fi
+
+echo "Clean yum repository"
+yum clean all
+
 # Remove results
 echo "Deleting results..."
-rm -rf /var/www/html/results/1*
+rm -rf /var/www/html/results/*
 
-# Clean tdnf repo
-tdnf clean all
+echo "Trimming disk space"
+fstrim /
+
+echo "Done."
