e55d2282d3c2f30ab3f5799c2dca1857f3e8e71f
Charles Lee <cleeistaken@gmail.com>
2022-02-09 00:00:31 -0600

    working on the vault scripts

---
+++ bin/vault-init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while getopts f: flag
+do
+  case "${flag}" in
+    f) force=${OPTARG};;
+  esac
+done
+
+echo "Warning: This script will reset the Hashicorp Vault tokens."
+echo "All currently stored data in vault will be lost."
+
+
+if [ -z ${force+x} ]; then
+  read -p "Are you sure? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "Exiting."
+    exit
+  fi
+else
+  echo "Skipping user prompt: '-f' flag set."
+fi
+
+pushd ../ansible > /dev/null
+  # Configure the systems
+  ANSIBLE_LOCALHOST_WARNING=false
+  export ANSIBLE_LOCALHOST_WARNING
+  ansible-playbook vault-init.yml
+popd > /dev/null
