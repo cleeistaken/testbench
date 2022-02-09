60b5089afa841539c684a95e9d50e319189bac91
Charles Lee <cleeistaken@gmail.com>
2022-02-09 00:18:58 -0600

    set mode to +x

--- bin/vault-init.sh
+++ bin/vault-init.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-while getopts f: flag
-do
-  case "${flag}" in
-    f) FORCE=true;;
-  esac
+# Ref. https://stackoverflow.com/questions/31974550/boolean-cli-flag-using-getopts-in-bash
+
+FORCE='false'
+
+# Process all options supplied on the command line
+while getopts ':x' 'OPTKEY'; do
+    case ${OPTKEY} in
+        'f')
+            # Update the value of the option x flag we defined above
+            FORCE='true'
+            ;;
+        *)
+            echo "UNIMPLEMENTED OPTION -- ${OPTKEY}" >&2
+            exit 1
+            ;;
+    esac
 done
+
+# [optional] Remove all options processed by getopts.
+shift $(( OPTIND - 1 ))
+[[ "${1}" == "--" ]] && shift
 
 echo "Warning: This script will reset the Hashicorp Vault tokens."
 echo "All currently stored data in vault will be lost."
 
-
-if [ -z ${FORCE+x} ]; then
+if ${FORCE}; then
+  echo "Skipping user prompt: '-f' flag set."
+else
   read -p "Are you sure? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -19,8 +35,6 @@ if [ -z ${FORCE+x} ]; then
     echo "Exiting."
     exit
   fi
-else
-  echo "Skipping user prompt: '-f' flag set."
 fi
 
 pushd ../ansible > /dev/null
@@ -28,3 +42,4 @@ pushd ../ansible > /dev/null
   ANSIBLE_LOCALHOST_WARNING=false
   export ANSIBLE_LOCALHOST_WARNING
   ansible-playbook vault-init.yml
+popd > /dev/null
